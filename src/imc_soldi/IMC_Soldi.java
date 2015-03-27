8253b12c208c936a646c0ca1add48d2f5cc0c581
41344391 <41344391@099220-P31S204.macklabs.br>
2015-03-27 20:58:35 -0300

    Add Project

---
+++ src/imc_soldi/IMC_Soldi.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imc_soldi;
+
+/**
+ *
+ * @author 41344391
+ */
+public class IMC_Soldi {
+
+    //Comando principal para execução do programa
+    public static void main(String[] args)
+    {
+      //Criação de um objeto ex da classe Ex2
+      
+      Ex2 ex = new Ex2();
+      //posição da tela ao abri-la
+      ex.setLocation(400,200);
+      //tornando a tela visível
+      ex.setVisible(true);
+   }
+}
